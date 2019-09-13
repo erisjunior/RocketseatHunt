e387536f5d0e9ddb644556bacf1aac60381bd662
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-09-12 23:07:43 -0300

    Hunt Project

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Ap from './src';
+
+export default class App extends React.Component {
+  render() {
+    return <Ap/>
+  }
+}
---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor("#DA225F");
+StatusBar.setBarStyle("light-content");
